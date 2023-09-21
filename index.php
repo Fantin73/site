a3664d30a1bd2cda0841f0885730d46dde7022e0
Darruiz <99142969+Darruiz@users.noreply.github.com>
2023-09-21 09:19:25 -0300

    p1

--- index.php
+++ index.php
@@ -6,45 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela Inicial</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style\navbar.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <?php include 'menus/menu1.php';?>
-    <div id="Navbar">
-    <main>
-		<div id="slider">
-
-			<div class="slider-inner">
-				<div id="slider-content">
-					<div class="meta">Senac Bauru</div>
-					<h2 id="slide-title">Turma 1</h2>
-					<span data-slide-title="0">Turma 1</span>
-					<span data-slide-title="1">Turma 1</span>
-					<div class="meta">Terceiro Colegial</div>
-					<div id="slide-status"></div>
-					<span data-slide-status="0"></span>
-					<span data-slide-status="1"></span>
-				</div>
-			</div>
-
-			<img src="Imagem1.jpg" />
-			<img src="Imagem2.jpg" />
-
-			<div id="pagination">
-				<button class="active" data-slide="0"></button>
-				<button data-slide="1"></button>
-			</div>
-
-		</div>
-	</main>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40525870-5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-40525870-5');
-</script>
+   
 
 	</body>
 
--- menus/menu1.php
+++ menus/menu1.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/menu1.css">
-    <title>Document</title>
+    <title>Home T1</title>
 </head>
 <body>
 
@@ -19,16 +19,7 @@
         <nav class="nav">
         <ul>
             <li class="nav-item navas"><a class="navas" href="index.php">Home T1</a></li>
-            <li class="nav-item dropdown">
-                <a class="navas" href="#">Serviços &#9660;</a>
-                <div class="dropdown-content">
-                    <a href="index.php">Página Inicial</a>
-                    <a href="Penultimo.php">Sobre Nós</a>
-                    <a href="Chat.php">Chat</a>
-                
-                </div>
-            </li>
-            <li class="nav-item"><a class="navas" href="#">Fale Conosco</a></li>
+            <li class="nav-item navas"><a class="navas" href="index.php">Sobre Nós</a></li>
             <li class="nav-item1"><a class="navas"  href="Chat.php">Chat</a></li>
             <li class="nav-item2"><a class="navas1" href="Login.php">Login</a></li>
         </ul>
